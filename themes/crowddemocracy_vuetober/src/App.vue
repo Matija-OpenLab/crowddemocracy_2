f37477856aaa3b2ded7f61e0a48f77689e8f7113
Matija-OpenLab <dykaajitam@gmail.com>
2020-02-29 16:45:17 +0100

    init

---
+++ themes/crowddemocracy_vuetober/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <Intro></Intro>
+  </div>
+</template>
+
+<script>
+import Intro from "./components/Intro.vue";
+
+export default {
+  name: "App",
+  components: {
+    Intro
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ themes/crowddemocracy_vuetober/src/components/Intro.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>CrowdDemocracy</h1>
+  </div>
+</template>
