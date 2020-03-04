31561e28e0895ec640068ac371ba3fb683e4196c
Matija-OpenLab <dykaajitam@gmail.com>
2020-03-04 18:21:04 +0100

    Made Landing page DOM

--- themes/crowddemocracy_vuetober/src/App.vue
+++ themes/crowddemocracy_vuetober/src/App.vue
@@ -1,28 +1,18 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <Intro></Intro>
-  </div>
+    <div id="app">
+        <Intro></Intro>
+    </div>
 </template>
 
 <script>
 import Intro from "./components/Intro.vue";
 
 export default {
-  name: "App",
-  components: {
-    Intro
-  }
+    name: "App",
+    components: {
+        Intro
+    }
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
--- themes/crowddemocracy_vuetober/src/components/Intro.vue
+++ themes/crowddemocracy_vuetober/src/components/Intro.vue
@@ -1,6 +1,25 @@
 <template>
-  <div>
-    <h1>CrowdDemocracy</h1>
-    <p>This is our OpenLab project page and it is awesome</p>
-  </div>
+    <div class="wrap">
+        <h1>CrowdDemocracy</h1>
+        <h3>
+            Na crowd democracy mozte dat svoj hlas novym navrhom alebo ho
+            odovzdat ludom ktorym doverujete
+        </h3>
+        <b-button>Registrovať sa</b-button>
+        <b-button>Prihlásiť sa</b-button>
+
+        <!-- sekcia na zobrazovanie aktualnych hlasovani -->
+        <section>
+            <p>Post..</p>
+        </section>
+    </div>
 </template>
+<script>
+export default {};
+</script>
+<style scoped>
+.wrap {
+    background-image: url(../../../../assets/camera.jpg);
+    height: 100vh;
+}
+</style>
