cfa9149a089a917db168555cb651c132447095f2
Matija-OpenLab <dykaajitam@gmail.com>
2020-03-05 19:45:27 +0100

    basic styling on LP

--- themes/crowddemocracy_vuetober/src/components/Intro.vue
+++ themes/crowddemocracy_vuetober/src/components/Intro.vue
@@ -1,27 +1,52 @@
 <template>
-    <div class="wrap">
-        <h1>CrowdDemocracy</h1>
-        <h3>
+  <div class="wrap">
+    <h1 class="text-center header">CrowdDemocracy</h1>
+    <b-container class="content">
+      <b-row>
+        <b-col>
+          <h3>
             Na crowd democracy mozte dat svoj hlas novym navrhom alebo ho
             odovzdat ludom ktorym doverujete
-        </h3>
-        <router-link to="/registration" tag="b-button"
-            >Registrovať sa</router-link
-        >
-        <b-button>Prihlásiť sa</b-button>
+          </h3>
+        </b-col>
 
-        <!-- sekcia na zobrazovanie aktualnych hlasovani -->
-        <section>
-            <p>Post..</p>
-        </section>
-    </div>
+        <b-col class="buttons">
+          <router-link to="/registration" tag="b-button">Registrovať sa</router-link>
+          <b-button>Prihlásiť sa</b-button>
+        </b-col>
+      </b-row>
+    </b-container>
+    <!-- sekcia na zobrazovanie aktualnych hlasovani -->
+    <section>
+      <p>Post..</p>
+    </section>
+  </div>
 </template>
 <script>
 export default {};
 </script>
 <style scoped>
 .wrap {
-    background-image: url(../../../../assets/camera.jpg);
-    height: 100vh;
+  /* background-image: url(../../../../assets/camera.jpg); */
+  background-image: url(../assets/backg.jpg);
+  background-repeat: no-repeat;
+  background-size: cover;
+  height: 100vh;
+}
+.content {
+  height: 75vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.buttons {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+.header {
+  line-height: 1em;
+  font-size: 5em;
+  padding-top: 1em;
 }
 </style>
