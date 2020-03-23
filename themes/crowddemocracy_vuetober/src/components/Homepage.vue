8733fadc443cb9d6bd6d92a2dfbab6d1163bacfa
Dominika Krullova <60384405+DominikaKrullova@users.noreply.github.com>
2020-03-23 18:58:40 +0100

    init change homepage

--- themes/crowddemocracy_vuetober/src/components/Homepage.vue
+++ themes/crowddemocracy_vuetober/src/components/Homepage.vue
@@ -1,21 +1,33 @@
 <template>
   <div class="wrap">
-    <h1>Crowddemocracy</h1>
-    <b-button :to="'/'" variant="danger">Odhlásiť sa</b-button>
-    <div class="communities" v-for="(communitie, index) in communities" :key="index">
-      <!--TENTO DIV JE CELA KOMUNITA-->
-      <div class="communitie" @click="redirectToComm(index)">
-        <p class="com-name">{{communitie.name}}</p>
-        <p class="com-count">{{communitie.user_count}}</p>
-        <p class="com-owner">{{communitie.owner}}</p>
-      </div>
-    </div>
-    <div class="tops">
-      <h2>Top Komunity</h2>
-      <div class="names" v-for="(communitie, index) in communities" :key="index">
-        <h4>{{communitie.name}}</h4>
-      </div>
-    </div>
+    <b-row>
+      <b-col cols="6" class="list">
+        <div class="tops">
+          <p>Zoznam komunít, kde si členom</p>
+          <div class="names" v-for="(communitie, index) in communities" :key="index">
+            <h4>{{communitie.name}}</h4>
+          </div>
+        </div>
+      </b-col>
+      <b-col>
+        <b-button :to="'/'" variant="danger">Odhlásiť sa</b-button>
+        <h1>Crowddemocracy</h1>
+        <b-row>
+          <b-col>
+            <div class="communities" v-for="(communitie, index) in communities" :key="index">
+              <!--TENTO DIV JE CELA KOMUNITA-->
+              <div class="communitie" @click="redirectToComm(index)">
+                <p class="com-count">{{communitie.user_count}}</p>
+                <p class="com-name">{{communitie.name}}</p>
+                <p class="com-owner">{{communitie.owner}}</p>
+              </div>
+            </div>
+          </b-col>
+          <b-col>2 of 3</b-col>
+          <b-col>3 of 3</b-col>
+        </b-row>
+      </b-col>
+    </b-row>
   </div>
 </template>
 <script>
@@ -43,14 +55,27 @@ export default {
 </script>
 <style scoped>
 .wrap {
-  background-color: lightgray;
+  background-color: #d6dbe0;
   height: 100vh;
 }
+.list {
+  width: 150px;
+}
 .communitie {
-  display: flex;
+  height: 250px;
+  width: 250px;
   justify-content: space-evenly;
   background-color: white;
-  border: 1px solid black;
+  border-radius: 15px;
   cursor: pointer;
+}
+.com-name {
+  text-align: center;
+  font-size: 15px;
+  font-weight: bold;
+}
+.com-count {
+  text-align: center;
+  font-size: 11px;
 }
 </style>
