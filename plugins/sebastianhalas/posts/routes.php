874d6f740fda1e50fe4c716b1aadd59b0f0ddbf6
root <root@Lola.localdomain>
2020-03-27 14:04:55 +0100

    backend

--- plugins/sebastianhalas/posts/routes.php
+++ plugins/sebastianhalas/posts/routes.php
@@ -28,6 +28,26 @@ Route::get('api/v1/posts/{id}/vote_no', function ($id) {
     }
     
 });
+Route::get('api/v1/posts/{id}/unvote_yes', function ($id) {
+    $posts = Posts::where('id',$id)->take(1)->get();
+    foreach($posts as $post)
+    {
+        $votes = $post->vote_yes;
+        $post->vote_yes = $votes - 1;
+        $post->save();
+    }
+    
+});
+Route::get('api/v1/posts/{id}/unvote_no', function ($id) {
+    $posts = Posts::where('id',$id)->take(1)->get();
+    foreach($posts as $post)
+    {
+        $votes = $post->vote_no;
+        $post->vote_no = $votes - 1;
+        $post->save();
+    }
+    
+});
 Route::get('api/v1/posts/community_id/{id}', function ($id) {
     $posts = Posts::where('community_id',$id)->get();
     return $posts;
