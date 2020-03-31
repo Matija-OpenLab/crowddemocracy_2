5fd203662724c11dc3b731fade0f32419cced5f8
Dominika Krullova <60384405+DominikaKrullova@users.noreply.github.com>
2020-03-31 16:36:42 +0200

    login layout

--- themes/crowddemocracy_vuetober/src/components/Login.vue
+++ themes/crowddemocracy_vuetober/src/components/Login.vue
@@ -7,12 +7,20 @@
         <b-button class="registration" to="/registration">Registruj sa</b-button>
       </b-col>
     </b-row>
-    <h1>Prihlásenie</h1>
+    <h1 class="login-text">Prihlásenie</h1>
     <form class="form">
-      <input type="text" placeholder="Prezývka / Email" />
-      <input type="password" placeholder="Heslo" />
-      <b-button type="submit">Prihlásiť</b-button>
+      <input class="placeholder" type="text" placeholder="Prezývka / Email" />
+      <input class="placeholder" type="password" placeholder="Heslo" />
+      <b-button class="login" type="submit">Prihlásiť</b-button>
     </form>
+    <b-row class="footer">
+      <b-col class="footer-col1">
+        &reg; 2020 všetky práva vyhradené
+        <span class="footer-logo">CROWD</span>DEMOCRACY
+      </b-col>
+      <b-col class="footer-col2">Sledujte nás na sociálnych sieťach</b-col>
+      <b-col class="footer-col3">social media</b-col>
+    </b-row>
   </div>
 </template>
 <script>
@@ -37,7 +45,6 @@ export default {};
   font-size: 14px;
   margin-top: 5px;
 }
-
 .button {
   width: 500px;
   margin-left: 0px;
@@ -46,7 +53,6 @@ export default {};
   width: 500px;
   color: white;
 }
-
 .question {
   width: 500px;
   margin-left: 700px;
@@ -55,5 +61,53 @@ export default {};
   text-align: right;
   margin-top: 4px;
   color: #330066;
+}
+.login {
+  background-color: #330066;
+  color: white;
+  width: 215px;
+  height: 60px;
+  margin-top: 40px;
+  line-height: 43px;
+}
+.login-text {
+  margin-top: 300px;
+  margin-left: 130px;
+  font-size: 55px;
+  font-weight: 50;
+}
+.form {
+  margin-left: 130px;
+  margin-top: 25px;
+  color: black;
+}
+.placeholder {
+  width: 500px;
+  margin-top: 15px;
+  height: 50px;
+  border: white;
+  color: black;
+  border-bottom: 1px solid #d7d7c1;
+}
+.footer {
+  margin-top: 180px;
+}
+.footer-col1 {
+  font-size: 14px;
+  color: #330066;
+  margin-left: 40px;
+  white-space: nowrap;
+}
+.footer-col2 {
+  font-size: 14px;
+  color: #330066;
+  margin-left: 115px;
+  text-align: right;
+}
+.footer-col3 {
+  color: white;
+}
+.footer-logo {
+  font-weight: 900;
 }
 </style>
