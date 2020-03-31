fe42bb65805c8e9160aeec1a3290d3509b5c9ad9
Dominika Krullova <60384405+DominikaKrullova@users.noreply.github.com>
2020-03-31 15:12:30 +0200

    registration layout

--- themes/crowddemocracy_vuetober/src/components/Registration.vue
+++ themes/crowddemocracy_vuetober/src/components/Registration.vue
@@ -1,32 +1,43 @@
 <template>
   <div class="wrap">
     <b-row class="navbar">
-      <b-col class="logo">logo</b-col>
+      <b-col class="logo">
+        <!--  <img src="url(logo.png/assets-dominka/menoAssetu.png)" alt /> -->
+      </b-col>
       <b-col class="question">Už máš účet?</b-col>
       <b-col class="button">
-        <b-button class="registration" to="/login">Prihlás sa</b-button>
+        <b-button class="login" to="/login">Prihlás sa</b-button>
       </b-col>
     </b-row>
 
     <h1 class="title">Registrácia</h1>
     <form class="form">
-      <input type="email" placeholder="Email" />
-      <input type="text" placeholder="Používateľské meno" />
-      <input type="password" placeholder="Heslo" />
-      <input type="password" placeholder="Potvrdenie hesla" />
+      <input class="placeholder" type="email" placeholder="Email" />
+      <input class="placeholder" type="text" placeholder="Používateľské meno" />
+      <input class="placeholder" type="password" placeholder="Heslo" />
+      <input class="placeholder" type="password" placeholder="Potvrdenie hesla" />
       <div class="check">
-        <input type="checkbox" />
-        Súhlasím so spracovaním údajov
+        <input class="checkbox" type="checkbox" />
+        <p class="check-text">Súhlasím so spracovaním údajov</p>
       </div>
 
       <span class="error"></span>
-      <b-button type="submit">Registrácia</b-button>
+      <b-button class="registration" type="submit">Registrácia</b-button>
     </form>
+    <b-row class="footer">
+      <b-col class="footer-col1">
+        &reg; 2020 všetky práva vyhradené
+        <span class="footer-logo">CROWD</span>DEMOCRACY
+      </b-col>
+      <b-col class="footer-col2">Sledujte nás na sociálnych sieťach</b-col>
+      <b-col class="footer-col3">social media</b-col>
+    </b-row>
   </div>
 </template>
 <script>
 export default {};
 </script>
+
 <style lang="css" scoped>
 .form input {
   display: block;
@@ -37,7 +48,7 @@ export default {};
 
 /*Dominikin code skopirovany z Intor.vue*/
 
-.registration {
+.login {
   border-color: #330066;
   color: #330066;
   background-color: white;
@@ -55,7 +66,13 @@ export default {};
   width: 500px;
   color: white;
 }
-
+.check {
+  display: flex;
+  margin-top: 30px;
+}
+.check-text {
+  margin: 0px;
+}
 .question {
   width: 500px;
   margin-left: 700px;
@@ -64,5 +81,53 @@ export default {};
   text-align: right;
   margin-top: 4px;
   color: #330066;
+}
+.title {
+  margin-top: 150px;
+  margin-left: 130px;
+  font-size: 55px;
+  font-weight: 50;
+}
+.form {
+  margin-left: 130px;
+  margin-top: 25px;
+  color: black;
+}
+.placeholder {
+  width: 500px;
+  margin-top: 15px;
+  height: 50px;
+  border: white;
+  color: black;
+  border-bottom: 1px solid #d7d7c1;
+}
+.registration {
+  background-color: #330066;
+  color: white;
+  width: 215px;
+  height: 60px;
+  margin-top: 20px;
+  line-height: 43px;
+}
+.footer {
+  margin-top: 180px;
+}
+.footer-col1 {
+  font-size: 14px;
+  color: #330066;
+  margin-left: 40px;
+  white-space: nowrap;
+}
+.footer-col2 {
+  font-size: 14px;
+  color: #330066;
+  margin-left: 115px;
+  text-align: right;
+}
+.footer-col3 {
+  color: white;
+}
+.footer-logo {
+  font-weight: 900;
 }
 </style>
