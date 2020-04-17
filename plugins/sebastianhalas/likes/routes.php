a35725cda66f0303e29f35e8e25849b97ceb6531
root <root@Lola.localdomain>
2020-04-17 13:41:45 +0200

    likes

--- plugins/sebastianhalas/likes/routes.php
+++ plugins/sebastianhalas/likes/routes.php
@@ -5,13 +5,24 @@ Route::group(['prefix' => 'api/v1/likes'], function() {
         $like = new Likes;
         $like->user_id = $uid;
         $like->likeable_id = $lid;
+        $like->like_value = 1;
         $like->save();
         return;
     });
     Route::get('unlike/{uid}/{lid}', function ($uid,$lid) {
+        $like = new Likes;
+        $like->user_id = $uid;
+        $like->likeable_id = $lid;
+        $like->like_value = 0;
+        $like->save();
+        return;
+    });
+    Route::get('remove_like/{uid}/{lid}', function ($uid,$lid) {
         $like = Likes::where('user_id',$uid)
                         ->where('likeable_id',$lid)
                         ->take(1);
         $like->delete();
     });
+    
+
 });
