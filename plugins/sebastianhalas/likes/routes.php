3cc608b130961c91acea227d921686296ce04208
Sebastian Halas <xd>
2020-04-27 20:37:16 +0200

    BE likes & posts bind

--- plugins/sebastianhalas/likes/routes.php
+++ plugins/sebastianhalas/likes/routes.php
@@ -1,5 +1,6 @@
 <?php
 use SebastianHalas\Likes\Models\Likes;
+use SebastianHalas\Posts\Models\Posts;
 Route::group(['prefix' => 'api/v1/likes'], function() {
     Route::get('like/{uid}/{lid}', function ($uid,$lid) {
         $like = new Likes;
@@ -7,6 +8,10 @@ Route::group(['prefix' => 'api/v1/likes'], function() {
         $like->likeable_id = $lid;
         $like->like_value = 1;
         $like->save();
+        $post = Posts::where('id',$lid)
+                        ->first();
+        $post->vote_yes =$post->vote_yes + 1;
+        $post->save();
         return;
     });
     Route::get('unlike/{uid}/{lid}', function ($uid,$lid) {
@@ -15,6 +20,10 @@ Route::group(['prefix' => 'api/v1/likes'], function() {
         $like->likeable_id = $lid;
         $like->like_value = 0;
         $like->save();
+        $post = Posts::where('id',$lid)
+                        ->first();
+        $post->vote_no =$post->vote_no + 1;
+        $post->save();
         return;
     });
     Route::get('remove_like/{uid}/{lid}', function ($uid,$lid) {
@@ -22,5 +31,21 @@ Route::group(['prefix' => 'api/v1/likes'], function() {
                         ->where('likeable_id',$lid)
                         ->take(1);
         $like->delete();
+        $post = Posts::where('id',$lid)
+                        ->first();
+        $post->vote_yes =$post->vote_yes - 1;
+        $post->save();
+        return;
+    });
+    Route::get('remove_unlike/{uid}/{lid}', function ($uid,$lid) {
+        $like = Likes::where('user_id',$uid)
+                        ->where('likeable_id',$lid)
+                        ->take(1);
+        $like->delete();
+        $post = Posts::where('id',$lid)
+                        ->first();
+        $post->vote_no = $post->vote_no - 1;
+        $post->save();
+        return;
     });
 });
