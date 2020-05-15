a944d89a84b2f5efcf2133c60aa7ccd65ff9e68a
Sebastian Halas <xd>
2020-05-15 14:55:08 +0200

    BE: like fix and code fix

--- plugins/sebastianhalas/likes/routes.php
+++ plugins/sebastianhalas/likes/routes.php
@@ -2,58 +2,46 @@
 use SebastianHalas\Likes\Models\Likes;
 use SebastianHalas\Posts\Models\Posts;
 Route::group(['prefix' => 'api/v1/likes'], function() {
-    Route::post('like/{uid}/{lid}', function ($uid,$lid) {
+    Route::post('vote_yes/{post_id}', function ($post_id) {
+        
+        $token = request()->input('token');
+        $userModel = JWTAuth::toUser($token);
+        $userID = $userModel->id;
+        
         $like = new Likes;
-        $like->user_id = $uid;
-        $like->posts_id = $lid;
+        $like->user_id = $userID;
+        $like->posts_id = $post_id;
         $like->like_value = 1;
         $like->save();
         
-        $post = Posts::where('id',$lid)
-                        ->first();
-        $post->vote_yes =$post->vote_yes + 1;
-        $post->save();
-        
         return;
     });
-    Route::post('unlike/{uid}/{lid}', function ($uid,$lid) {
+    Route::post('vote_no/{post_id}', function ($post_id) {
+        
+        $token = request()->input('token');
+        $userModel = JWTAuth::toUser($token);
+        $userID = $userModel->id;
+        
         $like = new Likes;
-        $like->user_id = $uid;
-        $like->posts_id = $lid;
-        $like->posts_id = 0;
+        $like->user_id = $userID;
+        $like->posts_id = $post_id;
+        $like->like_value = 0;
         $like->save();
         
-        $post = Posts::where('id',$lid)
-                        ->first();
-        $post->vote_no =$post->vote_no + 1;
-        $post->save();
-        
         return;
     });
-    Route::post('remove_like/{uid}/{lid}', function ($uid,$lid) {
-        $like = Likes::where('user_id',$uid)
-                        ->where('posts_id',$lid)
+    Route::post('remove_vote/{post_id}', function ($post_id) {
+        
+        $token = request()->input('token');
+        $userModel = JWTAuth::toUser($token);
+        $userID = $userModel->id;
+
+        $like = Likes::where('user_id',$userID)
+                        ->where('posts_id',$post_id)
                         ->take(1);
         $like->delete();
         
-        $post = Posts::where('id',$lid)
-                        ->first();
-        $post->vote_yes =$post->vote_yes - 1;
-        $post->save();
-        
         return;
     });
-    Route::post('remove_unlike/{uid}/{lid}', function ($uid,$lid) {
-        $like = Likes::where('user_id',$uid)
-                        ->where('posts_id',$lid)
-                        ->take(1);
-        $like->delete();
-        
-        $post = Posts::where('id',$lid)
-                        ->first();
-        $post->vote_no = $post->vote_no - 1;
-        $post->save();
-        
-        return;
-    });
+    
 });
